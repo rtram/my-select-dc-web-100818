0eaf563c0ca8a1003dad780cc688bc18a3c52664
Robin Tram <robin.tram@gmail.com>
2018-09-20 19:21:13 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,18 +1,22 @@
-def my_select(collection)
+def my_select(array)
   if block_given?
     i = 0 
+    new_array = []
   
     while i < array.length
-      yield array
+      new_array.push(yield array[i])
       i += 1 
     end 
     
-    array
+    new_array
   else 
     puts "Hey! No block was given!"
   end 
 end
 
+collection = [1, 2, 3, 4, 5]
 my_select(collection) do |n|
-  n.select()
+  if n.even?
+    return n
+  end 
 end 
